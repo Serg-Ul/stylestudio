19fd3141ff3eb38abfe057593e5df97ad86a195a
Serg-Ul <71600351+Serg-Ul@users.noreply.github.com>
2020-09-20 18:53:32 +0300

    project

---
+++ js/app.js
@@ -0,0 +1,46 @@
+
+$(document).ready(function () {
+    // $('body').fadeIn(1000); // Плавная загрузка
+    // Прелоадер
+    $('.loader-container').fadeOut(5000);
+
+    // Меню бургер
+    $('.header__burger').click(function () {
+        $('.header__burger, .header__menu').toggleClass('active');
+        $('body').toggleClass('lock')
+    });
+
+   // Слайдер
+    $('.slider').slick({
+        arrows: false,
+        dots: true,
+        adaptiveHeight: false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        speed: 500,
+        easing: 'ease-in-out',
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        pauseOnDotsHover: false,
+        pauseOnFocus: false,
+        pauseOnHover: true,
+        draggable: true,
+        swipe: true,
+        touchThreshold: 10,
+        waitForAnimate: true,
+        centerMode: false,
+        variableWidth: false,
+        rows: 1,
+        slidesPerRow: 1,
+        vertical: false,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    });
+});
